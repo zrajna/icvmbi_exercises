1c93b9e4e4dd1804a1ffec67e8dcbb2dfb5127ea
Zalan Rajna <zalan.rajna@oulu.fi>
2020-09-14 13:16:38 +0300

    python code skeletons

--- python/ex1_functions.py
+++ python/ex1_functions.py
@@ -1,12 +1,21 @@
 import numpy as np
 import cv2 as cv
 
-def equalize_histogram():
-    return
 
-def rgb_grayscale():
-    return
+def equalize_histogram(img):
+    """Returns img with equalized histogram"""
+    # Your code comes here
+    return img
 
-def enhance_image():
-    return
+
+def rgb_grayscale(img, weights):
+    """Returns grayscale img converted with weights"""
+    # Your code comes here
+    return img[..., 0].squeeze()
+
+
+def enhance_image(img):
+    """Returns enhanced version of img"""
+    # Your code comes here
+    return img
 
