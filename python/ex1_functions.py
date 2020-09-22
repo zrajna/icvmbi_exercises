b90da2c314aac6251ec47f157bfb9fe8d6de0c1d
Zalan Rajna <zalan.rajna@oulu.fi>
2020-09-22 17:21:08 +0300

    hint to change return

--- python/ex1_functions.py
+++ python/ex1_functions.py
@@ -5,16 +5,16 @@ import cv2 as cv
 def equalize_histogram(img):
     """Returns img with equalized histogram"""
     # Your code comes here
-    return img
+    return img  # This just makes the function work, change it as you see fit!
 
 
 def rgb_grayscale(img, weights):
     """Returns grayscale img converted with weights"""
     # Your code comes here
-    return img[..., 0].squeeze()
+    return img[..., 0].squeeze()  # This just makes the function work, change it as you see fit!
 
 
 def enhance_image(img):
     """Returns enhanced version of img"""
     # Your code comes here
-    return img
+    return img  # This just makes the function work, change it as you see fit!
